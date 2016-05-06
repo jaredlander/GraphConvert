53fcf7db3611c4329d701b22ae3163883a799ea2
Jared Lander <info@jaredlander.com>
2016-05-06 00:58:57 -0400

    Used the underscore functions from dplyr to satisfy CRAN notes.
    Explicitly called functions with :: and cleaned up documentation and namespace to correspond.

--- R/igraphConversion.r
+++ R/igraphConversion.r
@@ -4,25 +4,31 @@
 #' @author Jared P. Lander
 #' @aliases prepIgraphConversion
 #' @export prepIgraphConversion
-#' @importFrom igraph get.data.frame
-#' @importFrom magritrr "%<>%"
-#' @importFrom dplyr mutate n rename select
+#' @importFrom magrittr "%<>%" "%>%"
+#' @importFrom dplyr mutate n rename select mutate_ rename_
 #' @param g An igraph object
 #' @return A list holding a nodes data.frames and an edges data.frame.  The nodes data.frame has at minimum an id and label column plus any other node attributes.  The edges data.frame has at minimum an id, source and target columns plus any edge attributes.
 #' @examples 
 #' 
 #' data(flightGraph)
-#' prepIgraphForGexf(flightGraph)
+#' prepIgraphConversion(flightGraph)
 prepIgraphConversion <- function(g)
 {
+    # first make sure the nodes have names
+    if(is.null(igraph::V(g)$name))
+    {
+        igraph::V(g)$name <- as.character(igraph::V(g))
+    }
+    
     # make graph into a list of data.frames
-    gdata <- get.data.frame(g, what = "both")
+    gdata <- igraph::get.data.frame(g, what = "both")
     
     # rename vertices to nodes
     names(gdata)[1] <- 'nodes'
     
     # add a column to nodes to hold the IDs
-    gdata$nodes %<>% mutate(id=as.integer(as.factor(name))) %>% rename(label=name)
+    #gdata$nodes %<>% mutate(id=as.integer(as.factor(name))) %>% rename(label=name)
+    gdata$nodes <- gdata$nodes %>% mutate_(id=~as.integer(as.factor(name))) %>% rename_(label=~name)
     # extract node info for color, position, size, shape and color
     
     ## replace from and to in edges with their IDs
@@ -30,9 +36,10 @@ prepIgraphConversion <- function(g)
     nodesID <- gdata$nodes$id
     names(nodesID) <- gdata$nodes$label
     # now in the edges create the id columns and an edge id
-    gdata$edges %<>% mutate(id=sprintf('e%s', 1:n()), source=nodesID[from], target=nodesID[to]) %>%
+    #gdata$edges %<>% mutate(id=sprintf('e%s', 1:n()), source=nodesID[from], target=nodesID[to]) %>%
+    gdata$edges %<>% mutate(id=1:n()) %>% mutate_(source=~nodesID[from], target=~nodesID[to]) %>%
         # and drop the old one
-        select(-from, -to)
+        dplyr::select_(~-from, ~-to)
     
     return(gdata)
 }
@@ -44,7 +51,6 @@ prepIgraphConversion <- function(g)
 #' @author Jared P. Lander
 #' @aliases igraphToJson
 #' @export igraphToJson
-#' @importFrom jsonlite toJSON
 #' @param g An igraph object
 #' @return A string of json text representing the igraph object as an array holding an array each for nodes and edges.
 #' @examples 
@@ -54,9 +60,9 @@ prepIgraphConversion <- function(g)
 igraphToJson <- function(g)
 {
     # convert graph into a list of data.frames
-    gdata<- prepIgraphConversion(g)
+    gdata <- prepIgraphConversion(g)
     
-    return(toJSON(gdata))
+    return(jsonlite::toJSON(gdata))
 }
 
 
@@ -76,6 +82,8 @@ igraphToJson <- function(g)
 igraphToGexf <- function(g)
 {
     # convert graph into a list of data.frames
-    gdata<- prepIgraphConversion(g)
+    gdata <- prepIgraphConversion(g)
+    
+    
 }
 
